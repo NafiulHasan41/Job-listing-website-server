0c48faf8b6fb25fbfeb98d6823ad234423f3b806
NafiulHasan41 <nhashik40@gmail.com>
2024-05-15 19:58:25 +0600

    Completed the the deletion part part of my added job

--- index.js
+++ index.js
@@ -193,6 +193,14 @@ async function run() {
        
         res.send(result)
       })
+       
+      //deleting specific job
+      app.delete('/job/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await jobsCollection.deleteOne(query)
+        res.send(result)
+      })
 
 
  
